38505b93cc3c7b6e4e60ffcf6aea8d24e5f47113
Gerard de Jong <gerard@dejong.za.net>
2017-01-18 15:47:38 +0200

    Added Neuron class with firing mechanisim.

--- Sources/Neuron.swift
+++ Sources/Neuron.swift
@@ -1,3 +1,30 @@
 class Neuron {
-    
+    var synapses: [Neuron]?
+    var potential: Float = 0.0
+    var threshold: Float = 0.0
+
+    init () {}
+
+    init (initialPotential: Float, initialThreshold: Float) {
+        potential = initialPotential
+        threshold = initialThreshold
+    }
+
+    func stimulate(signal: Float) {
+        potential += signal
+        if(potential >= threshold) {
+            self.fire()
+        }
+    }
+
+    func fire() {
+        for neuron in synapses ?? [] { 
+            neuron.stimulate(signal: potential)
+        }
+        self.reset()
+    }
+
+    func reset() {
+        potential = 0.0
+    }
 }
--- Tests/NeuralNetworkTests/NeuronTests.swift
+++ Tests/NeuralNetworkTests/NeuronTests.swift
@@ -2,15 +2,35 @@ import XCTest
 @testable import NeuralNetwork
 
 class NeuralNetworkTests: XCTestCase {
-    
+    private var neuron = Neuron()
+
+    override func setUp() {
+        neuron.potential = 0.5
+        neuron.threshold = 0.75
+    }
+
     func testNeuron()  {
-        let n = Neuron()
-        XCTAssertNotNil(n)
+        XCTAssertNotNil(neuron)
+    }
+
+    func testNeuronStimulation()  {
+        let connectedNeuron = Neuron(initialPotential: 0.0, initialThreshold: 0.85)
+        neuron.synapses = [connectedNeuron]
+        neuron.stimulate(signal: 0.3)
+        XCTAssertTrue(connectedNeuron.potential == 0.8)
+    }
+
+    func testNeuronReset()  {
+        neuron.potential = 0.5
+        neuron.reset()
+        XCTAssertTrue(neuron.potential == 0.0)
     }
 
     static var allTests : [(String, (NeuralNetworkTests) -> () throws -> Void)] {
         return [
-            ("testNeuron", testNeuron)
+            ("testNeuron", testNeuron),
+            ("testNeuronStimulation", testNeuronStimulation),
+            ("testNeuronReset", testNeuronReset)
         ]
     }
 }
